2524465107cda74bbc7b8ae1b4538f360418a889
Japan-ai <ai.1110.small.pond@gmail.com>
2020-03-18 17:19:18 +0900

    gitignore

--- src/database/migrations/2020_03_18_072854_create_challengelist_table.php
+++ src/database/migrations/2020_03_18_072854_create_challengelist_table.php
@@ -30,6 +30,6 @@ class CreateChallengelistTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('=challengelist');
+        Schema::dropIfExists('challengelist');
     }
 }
